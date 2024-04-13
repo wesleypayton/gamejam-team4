3574fc9296c82d0961ded3a283b083db25032760
Basicpython <39888296+Basicpython@users.noreply.github.com>
2024-04-13 15:00:21 -0500

    bee swarm creation, remove pariticle systems on flower standins

--- Assets/Scripts/FlowerBees.cs
+++ Assets/Scripts/FlowerBees.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class FlowerBees : MonoBehaviour
 {
-	public ParticleSystem bees;
+	public GameObject swarm;
+	private float count;
+	private float increment;
 	
     void Start()
     {
-
+		count = 0;
+		increment = -1;
+		swarm.SetActive(false);
     }
+	
+	void Update(){
+		if(swarm.activeSelf && count == 50){
+			swarm.transform.Translate(Vector3.up * Time.deltaTime);
+			swarm.transform.Translate(Vector3.up * Time.deltaTime);
+			swarm.transform.Translate(Vector3.up * Time.deltaTime);
+			increment = increment * -1;
+		}
+		
+		if(swarm.activeSelf && count == 0){
+			swarm.transform.Translate(Vector3.down * Time.deltaTime);
+			swarm.transform.Translate(Vector3.down * Time.deltaTime);
+			swarm.transform.Translate(Vector3.down * Time.deltaTime);
+			increment = increment * -1;
+		}
+		
+		if(swarm.activeSelf){
+			count = count + increment;
+		}
+		
+	}
 	
 	// make sure the bee particle effects activate on the flower when in range
 	private void OnCollisionEnter(Collision collision){
-		bees.Play();
+		swarm.SetActive(true);
 	}
 }
