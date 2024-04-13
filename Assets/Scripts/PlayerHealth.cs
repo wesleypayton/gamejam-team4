ef569585f198c540882b62332fd11d049acb1851
sjohn65189 <70227912+sjohn65189@users.noreply.github.com>
2024-04-13 14:39:28 -0500

    updated player health script to be in accordance with the bee severity

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,8 +7,8 @@ public class PlayerHealth : MonoBehaviour
 {
     public float Health, MaxHealth;
 
+    private Bee bee;
     [SerializeField]
-    
     private HealthBarUI healthBar;
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,16 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+/*
+        if (bee.severity == Bee.SeverityLevel.Low) {
+            SetHealth(-1.5f);
+        } else if (bee.severity == Bee.SeverityLevel.Medium) {
+            SetHealth(-2.5f);
+        } else if (bee.severity == Bee.SeverityLevel.High) {
+            SetHealth(-3.5f);
+        }
+*/
+
         if (Input.GetKeyDown("m")) {
             SetHealth(-2.5f);
         }
